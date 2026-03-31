01e918c1a1a23f4c96a269e31e7d867b324ea926
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2026-03-31 15:48:31 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
-let audioCtx;
+let audioCtx; // global AudioContext
+
 function playBeep() {
+  // Only create AudioContext on first user gesture
   if (!audioCtx) audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Create oscillator + gain
   let osc = audioCtx.createOscillator();
   let gain = audioCtx.createGain();
   osc.connect(gain);
   gain.connect(audioCtx.destination);
 
   osc.type = "sine";
-  osc.frequency.value = 600;
-  gain.gain.setValueAtTime(0.2, audioCtx.currentTime);
+  osc.frequency.setValueAtTime(600, audioCtx.currentTime); // beep frequency
+  gain.gain.setValueAtTime(0.2, audioCtx.currentTime);     // volume
 
   osc.start();
-  osc.stop(audioCtx.currentTime + 0.2);
+  osc.stop(audioCtx.currentTime + 0.2); // 200ms beep
 }
